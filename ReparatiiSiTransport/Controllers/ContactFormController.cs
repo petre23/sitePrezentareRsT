476fa30c1575794976bbb690f9dce656116c0827
petre23 <petre23@users.noreply.github.com>
2018-08-06 22:08:45 +0300

    Added Forma programare

---
+++ ReparatiiSiTransport/Controllers/ContactFormController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReparatiiSiTransport.Controllers
+{
+    public class ContactFormController : Controller
+    {
+        // GET: ContactForm
+        public ActionResult ContactForm()
+        {
+            return View();
+        }
+    }
+}
