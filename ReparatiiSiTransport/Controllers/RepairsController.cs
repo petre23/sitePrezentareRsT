cf16b955055eb38d9197a477acb614d9a2633924
petre23 <petre23@users.noreply.github.com>
2018-08-06 21:37:54 +0300

    Initial commit

---
+++ ReparatiiSiTransport/Controllers/RepairsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReparatiiSiTransport.Controllers
+{
+    public class RepairsController : Controller
+    {
+        // GET: Repairs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
