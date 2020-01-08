d7cddb7f82ffa14fcb80707ee52b5e2bff8c2164
petre23 <petre23@users.noreply.github.com>
2020-01-08 22:28:38 +0200

    Added new pages/cards and updated homepage

---
+++ ReparatiiSiTransport/Controllers/HeatersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReparatiiSiTransport.Controllers
+{
+    public class HeatersController : Controller
+    {
+        // GET: Heaters
+        public ActionResult Incalzitoare_Webastro_Eberspacher()
+        {
+            return View("~/Views/Heaters/Incalzitoare-Webastro-Eberspacher.cshtml");
+        }
+    }
+}
---
+++ ReparatiiSiTransport/Controllers/TachographsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReparatiiSiTransport.Controllers
+{
+    public class TachographsController : Controller
+    {
+        // GET: Tachographs
+        public ActionResult Tahografe_Inteligente_Craiova()
+        {
+            return View("~/Views/Tachographs/Tahografe-Inteligente-Craiova.cshtml");
+        }
+    }
+}
